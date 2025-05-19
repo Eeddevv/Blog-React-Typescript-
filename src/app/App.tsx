9486840b074a88471f51b5e9f191e329677a66db
eddevv <edddevvv@gmail.com>
2025-05-19 11:50:48 +0300

    add stylelint, i18n lint

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,13 +2,11 @@ import './styles/index.scss';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
-import { useTranslation } from 'react-i18next';
 import { AppRouter } from './providers/Router';
 import useTheme from './providers/ThemeProvider/lib/useTheme';
 
 const App = () => {
   const { theme } = useTheme();
-  const { t } = useTranslation();
 
   return (
     <div className={classNames('app', {}, [theme])}>
